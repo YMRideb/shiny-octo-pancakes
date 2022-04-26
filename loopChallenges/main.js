010392b981b3ee901c2b4f1eb7d69028d9e6fd2d
YMRideb <103914884+YMRideb@users.noreply.github.com>
2022-04-25 21:37:15 -0700

    @empire factorials left to do 04/25/22

--- loopChallenges/main.js
+++ loopChallenges/main.js
@@ -2,14 +2,14 @@ for (i = 1; i <= 20; i++) {
     console.log(i);
 }
 console.log("Next: Countdown by 3");
-// BREAK
+
 for (i = 100; i > 0; i--) {
     if (i % 3 == 0) {
         console.log(i);
     }
 }
 console.log("Next: Print Sequence");
-//BREAK
+
 for (i = 4; i > -4; i -= 1.5) {
     console.log(i);
 }
@@ -19,4 +19,10 @@ for (i=0; i<=100;i++) {
     sum = sum + i;
     console.log(sum);
 }
-// console.log(sum);
+
+console.log("Next: Factorial");
+var product = undefined;
+for(i=0;i<=12;i++) {
+    product = i*i;
+    console.log(product);
+}
