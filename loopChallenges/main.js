9295d65600200248d20ad79419f7753c268c7f09
YMRideb <103914884+YMRideb@users.noreply.github.com>
2022-04-25 17:55:49 -0700

    @home fixed commit 04/25/22

--- loopChallenges/main.js
+++ loopChallenges/main.js
@@ -10,6 +10,13 @@ for (i = 100; i > 0; i--) {
 }
 console.log("Next: Print Sequence");
 //BREAK
-for (i = 4; i > -4; i - 1.5) {
+for (i = 4; i > -4; i -= 1.5) {
     console.log(i);
 }
+console.log("Next: Sigma");
+var sum = 0;
+for (i=0; i<=100;i++) {
+    sum = sum + i;
+    console.log(sum);
+}
+// console.log(sum);
