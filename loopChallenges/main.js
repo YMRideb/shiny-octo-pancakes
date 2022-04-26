0cf44dbf68f4f5dd8472231d3bb88aa65e990470
YMRideb <103914884+YMRideb@users.noreply.github.com>
2022-04-25 17:26:04 -0700

    @home reset commit fix? 04/25/22

---
+++ loopChallenges/main.js
@@ -0,0 +1,15 @@
+for (i = 1; i <= 20; i++) {
+    console.log(i);
+}
+console.log("Next: Countdown by 3");
+// BREAK
+for (i = 100; i > 0; i--) {
+    if (i % 3 == 0) {
+        console.log(i);
+    }
+}
+console.log("Next: Print Sequence");
+//BREAK
+for (i = 4; i > -4; i - 1.5) {
+    console.log(i);
+}
