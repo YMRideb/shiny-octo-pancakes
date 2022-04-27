8c159810c78445d87dee99bb861fa768be338653
YMRideb <103914884+YMRideb@users.noreply.github.com>
2022-04-27 00:32:22 -0700

    @home going to sleep commit 04/27/22

--- Yukio_Rideb_WebFund_BeltExam/main.js
+++ Yukio_Rideb_WebFund_BeltExam/main.js
@@ -1,1 +1,5 @@
 console.log("there is no spoon")
+console.log("Shawn says to keep your Likes assignment handy for the Exam");
+console.log("W2D2 20:37");
+console.log("\'Even if it\'s not styled properly, we should [at least] have every word, every picture, every div\'");
+console.log("W2D2 28:30");
