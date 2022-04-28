08771cedc241b94b9d54b78f9c005e16c5e15246
YMRideb <103914884+YMRideb@users.noreply.github.com>
2022-04-28 12:27:36 -0700

    @home final commit b4 CSS 04/28/22

--- Yukio_Rideb_WebFund_BeltExam/yellowBelt/dsnn/dojo.js
+++ Yukio_Rideb_WebFund_BeltExam/yellowBelt/dsnn/dojo.js
@@ -1,1 +1,39 @@
 console.log('There is no spoon');
+function myFunction() {
+    timeout = setTimeout(alertFunc, 3000);
+  }
+  
+  function alertFunc() {
+    alert("Hello!");
+  }
+
+var count = 314;
+var ninjas = document.querySelector("#ninja-now");
+
+function add1() {
+    count++;
+    ninjas.innerText = count;
+}
+
+var count2 = 159;
+var pirates = document.querySelector("#pirate-now");
+
+function addOne(element) {
+    count2++;
+    pirates.innerText = count2;
+}
+
+function subscribe() {
+    var elem = document.querySelector('#boom');
+    elem.remove();
+}
+
+let timeout;
+
+function endGame() {
+  timeout = setTimeout(alertFunc, 13000);
+}
+
+function alertFunc() {
+  alert("Ninjas for the Win!!");
+}
