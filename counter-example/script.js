6e56f95b5dd717bf79fcc4d9fd08751777a390b3
YMRideb <103914884+YMRideb@users.noreply.github.com>
2022-04-27 20:25:09 -0700

    @empire save new file commit 04/27/22

--- counter-example/script.js
+++ counter-example/script.js
@@ -1,29 +1,29 @@
-console.log("test");
-var count = 0;
-var countNum = document.querySelector("#count");
+// console.log("test");
+// var count = 0;
+// var countNum = document.querySelector("#count");
 
-console.log(countNum);
-function add1() {
-    count++;
-    countNum.innerText = "The count is " + count
-    console.log(count);
-}
+// console.log(countNum);
+// function add1() {
+//     count++;
+//     countNum.innerText = "The count is " + count
+//     console.log(count);
+// }
 
-function sub1() {
-    count--;
-    countNum.innerText = "The count is " + count
-    console.log(count);
-}
+// function sub1() {
+//     count--;
+//     countNum.innerText = "The count is " + count
+//     console.log(count);
+// }
 
-var picture = document.querySelector(#womanImg);
+// var pict = document.getElementById.src(#womanImg);
 
-function swapImg() {
-    picture.src = "womanPeep.jpg"
-    // console.log(picture);
-}
+// function swapImg() {
+//     document.getElementById(#womanImg).src = "../images/manBacksplash.jpg";
+//     // console.log(picture);
+// }
 
-function swapBack() {
-    picture.src = "manBacksplash.jpg.jpg"
-    // console.log(picture);
-}
-console.log(picture.src);
+// function swapBack() {
+//     pict.innerHTML = "../images/womanPeep.jpg"
+//     // console.log(picture);
+// }
+// console.log(picture.src);
