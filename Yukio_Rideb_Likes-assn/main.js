0bddf4ce07ddde2c278a4d6f40ed2f28d86bc2f0
YMRideb <103914884+YMRideb@users.noreply.github.com>
2022-04-27 22:56:51 -0700

    @empire starting phase 2 04/27/22

--- Yukio_Rideb_Likes-assn/main.js
+++ Yukio_Rideb_Likes-assn/main.js
@@ -1,8 +1,12 @@
-console.log("There is no spoon");
-function addLike() {
-    console.log("This is a test. Test");
-}
+var count = 0;
+var countNum = document.querySelector("#counter");
 
 function add1() {
-    
+    count++;
+    countNum.innerText = count + " Likes";
+}
+
+function sub1() {
+    count = 0;
+    countNum.innerText = count + " Likes";
 }
