950b24573036e9525c9a10796566f0982383b1f7
YMRideb <103914884+YMRideb@users.noreply.github.com>
2022-04-28 02:38:00 -0700

    @home more edits commit 04/28/22

--- Yukio_Rideb_Likes-assn/main.js
+++ Yukio_Rideb_Likes-assn/main.js
@@ -6,7 +6,25 @@ function add1() {
     countNum.innerText = count + " Likes";
 }
 
-function sub1() {
+var count2 = 0;
+var countNum2 = document.querySelector("#counter2");
+
+function addOne() {
+    count++;
+    countNum2.innerText = count + " Likes";
+}
+var count3 = 0;
+var countNum3 = document.querySelector("#counter3");
+
+function add_1() {
+    count++;
+    countNum3.innerText = count + " Likes";
+}
+function plusOne() {
+    count++;
+    countNum4.innerText = count + " Likes";
+}
+function subAll() {
     count = 0;
     countNum.innerText = count + " Likes";
 }
