544f3e004dca322bc325f4ceeb739e431aa5238f
YMRideb <103914884+YMRideb@users.noreply.github.com>
2022-08-31 12:18:15 -0700

    @home finished first_jsx commit

--- aug_sept/Platform/jsx/first_jsx/first_jsx/src/App.js
+++ aug_sept/Platform/jsx/first_jsx/first_jsx/src/App.js
@@ -5,20 +5,14 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="my-class">This is JSX</h1>
+      <h2>Things I need to do:</h2>
+      <ol>
+        <li>Learn React</li>
+        <li>Go Skiing this winter</li>
+        <li>Skate in the snakerun by next summer</li>
+        <li>Deploy a project</li>
+      </ol>
     </div>
   );
 }
