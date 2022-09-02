ab9b10d33401f17c10f6793ed18f5145dae3d580
YMRideb <103914884+YMRideb@users.noreply.github.com>
2022-09-02 07:55:52 -0700

    @home finished putting it together commit

--- aug_sept/Platform/jsx/first_jsx/first_jsx/src/components/UserCard.jsx
+++ aug_sept/Platform/jsx/first_jsx/first_jsx/src/components/UserCard.jsx
@@ -1,12 +1,21 @@
 import { Component } from "react";
 
 class UserCard extends Component {
+  constructor(props){
+    super(props);
+    // a special type of variable that can we can change,
+    //and when it does change the component reloads on the screen with the newly changed information
+    this.state = {
+      userAge: this.props.userAge
+  };
+}
   render() {
     return (
       <div className="User">
         <h2>Name: {this.props.firstName} {this.props.lastName} </h2>
-        <p>Age: {this.props.userAge}</p>
+        <p>Age: {this.state.userAge}</p>
         <p>Hair Color: {this.props.userHair}</p>
+        <button onClick={() =>this.setState({userAge: this.state.userAge+1})}>Click this button for: {this.props.firstName}</button>
       </div>
     );
   }
