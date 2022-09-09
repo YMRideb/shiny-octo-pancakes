7ff98426b7e38c2199d75c5bd70133df811a303c
YMRideb <103914884+YMRideb@users.noreply.github.com>
2022-09-08 23:37:22 -0700

    @home practice pokemon commit

--- aug_sept/Extras/psuedocode.md
+++ aug_sept/Extras/psuedocode.md
@@ -1,1 +1,8 @@
 APIs can return an array of objects back to you
+
+The following snippet is for adding pretty globally
+You can add
+
+DBQuery.prototype._prettyShell = true
+
+to your file in $HOME/.mongorc.js to enable pretty print globally by default.
--- aug_sept/Platform/apis/pokedex/src/components/fetchpokemon.jsx
+++ aug_sept/Platform/apis/pokedex/src/components/fetchpokemon.jsx
@@ -1,18 +1,43 @@
-import React from "react";
+import React, {useState} from "react";
 
-const FetchPokemon = ()=>{
-    fetch("www.pokedexapi.com")
-    .then(response=>{
-        return response.json()
-    })
-    .then(response=>{
-        console.log("response from api is ->", response)
-    })
+const FetchPokemon = () => {
+  let [pokemon, setPokemon] = useState([]);
 
-    return(
-        <div>
-            <button>This is a button</button>
-        </div>
-    )
-}
+  const getData = () => {
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=2000")
+      .then((response) => {
+        return response.json();
+      })
+      .then((response) => {
+        console.log("response from api is ->", response);
+        setPokemon(response.results);
+        // console.log(response);
+      })
+      .catch((err) => {
+        console.log("something went wrong with your response");
+      });
+  };
+
+  console.log("This is me waiting for the api call to return");
+  console.log("like a dog fetching a ball");
+
+  return (
+    <div>
+      <button onClick={getData}>This is a button</button>
+      {pokemon.map((Pokemon, idx) => {
+        return (
+          <div className="container" key={idx}>
+            <div className="card card-body">
+              <ul>
+                <li>{Pokemon.name}</li>
+              </ul>
+              {/* <h2>{pokemon.type}</h2>
+                        <h2>{pokemon.size}</h2> */}
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 export default FetchPokemon;
