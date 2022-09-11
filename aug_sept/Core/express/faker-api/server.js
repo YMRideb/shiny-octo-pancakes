76d48512339daf58d68bc26f8931ebfc205d5146
YMRideb <103914884+YMRideb@users.noreply.github.com>
2022-09-11 00:05:20 -0700

    @empire faker db install commit

--- aug_sept/Core/express/faker-api/server.js
+++ aug_sept/Core/express/faker-api/server.js
@@ -4,8 +4,16 @@ const port = 8000;
 
 //get, post, put, delete
 
+// const UserList = [
+//     {_id: 1, firstName: "James", lastName: "Holden", email: "captain@roci.net", password: "qwerty12345"}
+// ]
+
+// const CompanyList = [
+//     {_id: 1, name: "Rocinante Gas Freighter", address: {street: "Fifth Level", city: "Anti-spinward", state: "The Belt", zipCode: "99999", country: "Sol"}}
+// ]
+
 app.get("/api",(req, res)=>{
-    res.json({message: "there is no spoon"})
+    res.json({message: "wubba lubba dub dubs"})
 
 })
 
